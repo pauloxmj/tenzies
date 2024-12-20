60dbc2153c0e780c62f404b98605f1afc18eb554
Paulo Ximenes <pauloxmj@gmail.com>
2024-12-20 14:19:09 -0300

    Added win conditions and winning effect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,67 @@
-import { useState } from 'react'
-import styles from './App.module.css'
-import Die from './Die'
+import { useState } from 'react';
+import styles from './App.module.css';
+import Die from './Die';
+import { nanoid } from 'nanoid';
+import Confetti from 'react-confetti';
 
 export default function App() {
+  const [dice, setDice] = useState(generateAllNewDice);
+
+ const gameWon = dice.every(die => die.isHeld) && 
+  dice.every(die => die.value === dice[0].value);
+  
   function generateAllNewDice() {
-      return new Array(10).fill(0).map(() => Math.ceil(Math.random() * 6));
+      return new Array(10)
+      .fill(0)
+      .map(() => ({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid()
+      }));
   }
-  const [diceArray, setDiceArray] = useState(generateAllNewDice);
   
   function rollDice() {
-    setDiceArray(generateAllNewDice());
+    if (gameWon) {
+      setDice(generateAllNewDice());
+    } else {
+      setDice(prevDice =>
+        prevDice.map(die =>
+          die.isHeld ? die : { ...die, value: Math.ceil(Math.random() * 6) }
+        )
+      );
+    }
   }
+
+  function hold(id) {
+    setDice(prevDice =>
+      prevDice.map(die =>
+        die.id === id? {...die, isHeld: !die.isHeld}
+        : die
+      )
+    )
+  }
+
+  const diceElements = dice.map(diceObj => (
+    <Die 
+      key={diceObj.id} 
+      value={diceObj.value} 
+      isHeld={diceObj.isHeld}
+      hold={() => hold(diceObj.id)}
+      />
+    ));
 
   return (
     <main>
+      {gameWon && <Confetti />}
       <div className={styles.container}>
+        <h1 className={styles.title}>Tenzies</h1>
+        <p className={styles.instructions}>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
         <div className={styles.diceContainer}>
-          {diceArray.map(
-            (value, index) => (<Die key={index} value={value} />)
-          )}
+          {diceElements}
         </div>
-        <button onClick={rollDice}>Roll Dice</button>
+        <button className={styles.rollButton} onClick={rollDice}>
+          {gameWon ? "New Game" : "Roll"}
+        </button>
       </div>
     </main>
   )
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,7 +1,17 @@
-import styles from './Die.module.css';
+import styles from "./Die.module.css";
 
-export default function Die(props) {  
-    return (
-        <button className={styles.dice}>{props.value}</button>
-    )
+export default function Die({ value, isHeld, hold }) {
+  const style = {
+    backgroundColor: isHeld ? "#59E391" : "#FFFFFF",
+  };
+
+  return (
+    <button 
+    className={styles.dice} 
+    style={style}
+    onClick={hold}
+    >
+      {value}
+    </button>
+  );
 }
